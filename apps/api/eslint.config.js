4d102692177475a7c1555912fb35789c90ce0409
Jun Son <31465911+juniverse1103@users.noreply.github.com>
2025-06-20 22:45:29 +0900

    chore: remove deprecated husky lines and cleanup api eslint config

--- apps/api/eslint.config.js
+++ apps/api/eslint.config.js
@@ -1,21 +1,13 @@
-// ESLint Flat Config for NestJS API (ESLint v9+)
-const js = require('@eslint/js');
-const tseslint = require('typescript-eslint');
-
-module.exports = [
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    files: ['src/**/*.ts'],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        project: './tsconfig.json',
-      },
-    },
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'warn',
-      'no-console': 'off',
-    },
-  },
-];
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  env: {
+    node: true,
+    es2021: true
+  }
+};
